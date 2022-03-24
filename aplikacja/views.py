e6d659af986700115e651bf3d523dccb98832f24
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-03-24 15:02:02 +0100

    query_set=Product.objects.values('title').filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')

--- aplikacja/views.py
+++ aplikacja/views.py
@@ -1,15 +1,15 @@
-from django.db.models import Q
+from django.db.models import Q, F
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product,OrderItem
 
 
 # Create your views here.
 
 
 def sey_hello(request):
-    query_set = Product.objects.filter((Q(id__gt=14) | Q(inventory__lt=10)) & ~Q(unit_price__lt=20))
+    query_set=Product.objects.values('title').filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
-    for product in query_set:
-        print(product.title)
+    # for product in query_set:
+    #     print(product.title)
 
     return render(request, "html.html", {'name': 'Ja', 'products': query_set})
