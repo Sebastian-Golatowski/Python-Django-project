0f35f864753b6eb5a0e350ee4caa0af3974c3e21
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-03-24 22:15:11 +0100

    .select_related().prefetch_related()

--- aplikacja/views.py
+++ aplikacja/views.py
@@ -1,15 +1,14 @@
 from django.db.models import Q, F
 from django.shortcuts import render
-from store.models import Product,OrderItem
+from store.models import Product,Order
 
 
 # Create your views here.
 
 
 def sey_hello(request):
-    query_set=Product.objects.values('title').filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
-
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
     # for product in query_set:
     #     print(product.title)
 
-    return render(request, "html.html", {'name': 'Ja', 'products': query_set})
+    return render(request, "html.html", {'name': 'Ja', 'orders': query_set})
