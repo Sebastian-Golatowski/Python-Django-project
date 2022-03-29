352ecdc963a680908307ba3a45ceeaea900e89f2
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-03-29 20:11:18 +0200

    koniec cz.1

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
-
 from . import models
 
 
--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,8 @@
+from re import search
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['label']
+
+admin.site.register(models.Tag,TagAdmin)
