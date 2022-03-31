c17526d5f5d2e059aaa3a66bafbf3882284f4b41
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-03-31 15:25:13 +0200

    serializing relationships: pk, string, nested objects, hyperlink

---
+++ store/serializer.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from gc import collect
+from turtle import title
+from store.models import Product, Collection
+from rest_framework import serializers
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2,source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name = 'collection-detail',
+    )
+    
+    def calculate_tax(self, product):
+        return round(product.unit_price * Decimal(1.1),2)
--- store/views.py
+++ store/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from decimal import Context
+from django.shortcuts import get_list_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product, Collection
+from .serializer import ProductSerializer
+from store import serializer
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    query_set= Product.objects.select_related('collection').all()
+    serializer=ProductSerializer(query_set, many=True, context={'request':request})
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_list_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view()    
+def collection_detail(request, pk):
+    return Response('ok')
+    
