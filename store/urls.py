870f1e1f900227a13c1a27c9cd42433e56beb3c5
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-04-06 11:23:50 +0200

    def vievs -> class views

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from django.views import View
 from . import views
 
 urlpatterns = [
-    path("products/",views.product_list),
-    path("products/<int:id>/",views.product_detail),
+    path("products/",views.ProductList.as_view()),
+    path("products/<int:id>/",views.ProductDetail.as_view()),
     path("collections/",views.collections_list),
     path("collections/<int:id>",views.collection_detail)
 ]
